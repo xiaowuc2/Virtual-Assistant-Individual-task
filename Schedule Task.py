1c53b70155a2e9e391cc6aa23b7d2d8afcdd109f
xiaowuc2 <50553530+xiaowuc2@users.noreply.github.com>
2020-05-22 15:18:10 +0530

    Update Schedule Task.py

--- Schedule Task.py
+++ Schedule Task.py
@@ -12,13 +12,13 @@ def b():
 def c():
     print("now you are")
     
-#-----------------------------
+#--------------------------------------------
 
 schedule.every(2).seconds.do(a)
 schedule.every(4).seconds.do(b)
 schedule.every().day.at("10:55").do(c)
 
-#----------------------------------------------
+#---------------------------------------------
 while 1:
     schedule.run_pending()
     time.sleep(1)
